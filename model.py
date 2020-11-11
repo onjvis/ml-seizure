221e3c6a06fe825f58347a2f5e458c7d2c694bcb
David <dmarquez.estudis@gmail.com>
2020-11-11 11:36:56 +0100

    Corss validation for Kneighbours

--- model.py
+++ model.py
@@ -5,8 +5,53 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing, metrics
 import matplotlib.pyplot as plt 
+from abc import ABC, abstractmethod
 
-def getFoldData(fold_data, dataType, labelEncoder):
+class SeizureClassification(ABC):
+    def run(self, cross_val_file, data_dir):
+        sz = pickle.load(open(cross_val_file, "rb"))
+    
+        szr_type_list = ['TNSZ', 'SPSZ', 'ABSZ', 'TCSZ', 'CPSZ', 'GNSZ', 'FNSZ']
+        le = preprocessing.LabelEncoder()
+        le.fit(szr_type_list)
+        
+        results = []
+        originalLabels = []
+        predictedLabels = []
+        # Iterate through the folds
+        for i, fold_data in enumerate(sz.values()):
+        
+            # fold_data is a dictionary with train and val keys
+            # Each contains a list of name of files
+        
+            X_train, y_train = getFoldData(data_dir, fold_data, "train", le)
+            X_test, y_test = getFoldData(data_dir, fold_data, "val", le)
+            model = self._generateModel()
+
+            clf = model.fit(X_train, y_train)
+            predicted = model.predict(X_test)
+            originalLabels.extend(y_test)
+            predictedLabels.extend(predicted)
+            score = clf.score(X_test, y_test)
+            results.append(score)
+            print("Fold number ",i, " completed. Score: ", score)
+        print("Classification report for classifier %s:\n%s\n"
+      % ("Test", metrics.classification_report(originalLabels, predictedLabels)))
+        
+        print("Avg result: ", np.mean(results) )
+    
+    @abstractmethod
+    def _generateModel(self):
+        pass
+        
+            
+class KNeighboursClassification(SeizureClassification):
+    def _generateModel(self):
+        return KNeighborsClassifier()
+    
+
+
+def getFoldData(data_dir, fold_data, dataType, labelEncoder):
     X = list()#np.empty(len(fold_data))
     y = list()#np.empty(len(fold_data))
         
@@ -14,11 +59,7 @@ def getFoldData(fold_data, dataType, labelEncoder):
         # each file contains a named tupple
         # 'patient_id','seizure_type', 'data'
         seizure = pickle.load(open(os.path.join(data_dir, fname), "rb"))
-        #if(not isinstance(seizure.data, np.ndarray) or seizure.data.ndim != 2):
-        #    print("HELLO")
         X.append(np.sum(seizure.data, axis=0))
-        #if(np.array(X_train).ndim > 1):
-        #    print("DMIM")
         
         y.append(seizure.seizure_type)
     y = labelEncoder.transform(y)
@@ -33,34 +74,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     cross_val_file = args.cross_val_file
     data_dir = args.data_dir
-    sz = pickle.load(open(cross_val_file, "rb"))
-    szr_type_list = ['TNSZ', 'SPSZ', 'ABSZ', 'TCSZ', 'CPSZ', 'GNSZ', 'FNSZ']
     
-    le = preprocessing.LabelEncoder()
-    le.fit(szr_type_list)
-    
+    a = KNeighboursClassification()
+    a.run(cross_val_file, data_dir)
+   
 
-    # Iterate through the folds
-    for fold_data in sz.values():
-    
-        # fold_data is a dictionary with train and val keys
-        # Each contains a list of name of files
-       
-        X_train, y_train = getFoldData(fold_data, "train", le)
-        X_test, y_test = getFoldData(fold_data, "val", le)
-        neigh = KNeighborsClassifier()
-
-        clf = neigh.fit(X_train, y_train)
-        predicted = neigh.predict(X_test)
-        print("Classification report for classifier %s:\n%s\n"
-      % (neigh, metrics.classification_report(y_test, predicted)))
-        disp = metrics.plot_confusion_matrix(neigh, X_test, y_test, display_labels=le.classes_)
-        plt.show() 
-        disp.figure_.suptitle("Confusion Matrix")
-        print("Confusion matrix:\n%s" % disp.confusion_matrix)
-        print(clf.score(X_test, y_test))
-        #print(y_train[-1])
-        #print(neigh.predict([X_train[-1]]))
-        #print(neigh.predict_proba([X_train[-1]]))
-        exit() # To end in the first run
-    
+#python3 model.py -c ./data_preparation/cv_split_3_fold_patient_wise_v1.5.2.pkl -d "/media/david/Extreme SSD/Machine Learning/raw_data/v1.5.2/fft_with_time_freq_corr/fft_seizures_wl1_ws_0.5_sf_250_fft_min_1_fft_max_12"
